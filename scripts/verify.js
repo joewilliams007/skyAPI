4eeb9158c51cf20078c2b18ed4452da6f8dc49c1
Universe <williamsthehack@gmail.com>
2023-05-18 15:57:04 +0200

    up

--- scripts/verify.js
+++ scripts/verify.js
@@ -9,27 +9,43 @@ module.exports = (req, res) => {
     // why can the key just be the timestamp? isnt that insecure?
     // -> when sky verifies the key, the key must posted from the correct user_id. thus only the real owner can use the key to verify his session
 
-    db.query(
-        `INSERT INTO Auth (session_id, user_id, verify_key) 
-        VALUES ("${session_id}",${user_id},${timestamp})`
-        , function (error, results, fields) {
-            if (error) {
+    var getJSON = require('get-json')
+    var db = require('./db');
 
-                console.log('insert Auth error ' + error.message);
+    // fetch profile details such as username, avatar, color
+    getJSON('https://devrant.com/api/users/'+user_id+'?app=3/')
+        .then(function (response) {
 
-                res.status(200).json({
-                    success: false,
-                    error: true,
-                    message: error.message
-                })
+            db.query(
+                `INSERT INTO Auth (session_id, user_id, verify_key, username, avatar, color) 
+                VALUES ("${session_id}",${user_id},${timestamp},"${response.profile.username}","${response.content.avatar_sm.i}","${response.content.avatar_sm.b}")`
+                , function (error, results, fields) {
+                    if (error) {
+        
+                        console.log('insert Auth error ' + error.message);
+        
+                        res.status(200).json({
+                            success: false,
+                            error: true,
+                            message: error.message
+                        })
+        
+                    } else {
+                        res.status(200).json({
+                            success: true,
+                            error: false,
+                            verify_key: timestamp,
+                            message: "session id can be verified by commenting verify_key to the verify rant"
+                        })
+                    }
+            });
 
-            } else {
-                res.status(200).json({
-                    success: true,
-                    error: false,
-                    verify_key: timestamp,
-                    message: "session id can be verified by commenting verify_key to the verify rant"
-                })
-            }
+    }).catch(function (error) {
+        console.log(error);
+
+        res.status(200).json({ 
+            success: false,
+            error: true,
+            message: "error\n\n"+error })
     });
 }
