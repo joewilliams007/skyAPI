d4863d3beed20213a05a28e136267ca68135757b
Universe <williamsthehack@gmail.com>
2023-05-18 16:00:25 +0200

    Update verify.js

--- scripts/verify.js
+++ scripts/verify.js
@@ -18,7 +18,7 @@ module.exports = (req, res) => {
 
             db.query(
                 `INSERT INTO Auth (session_id, user_id, verify_key, username, avatar, color) 
-                VALUES ("${session_id}",${user_id},${timestamp},"${response.profile.username}","${response.profile.content.avatar_sm.i}","${response.profile.content.avatar_sm.b}")`
+                VALUES ("${session_id}",${user_id},${timestamp},"${response.profile.username}","${response.profile.user_avatar.i}","${response.profile.user_avatar.b}")`
                 , function (error, results, fields) {
                     if (error) {
         
