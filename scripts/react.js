fdacb3c5c7330eb414db828870f0aafdb42e47ce
Universe <williamsthehack@gmail.com>
2023-05-19 14:27:32 +0200

    Update react.js

--- scripts/react.js
+++ scripts/react.js
@@ -37,6 +37,7 @@ module.exports = (req, res) => {
                         setReaction();
                     } else {
                         deleteReaction();
+                        setReaction();
                     }
 
                 }
