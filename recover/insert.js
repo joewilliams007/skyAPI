30902d9c950e1caf05a805fa15b07f5dcc4db837
Universe <williamsthehack@gmail.com>
2023-05-23 20:39:50 +0200

    Update insert.js

--- recover/insert.js
+++ recover/insert.js
@@ -4,5 +4,9 @@ fs.readFile('./community.json', 'utf8', function (err, data) {
   if (err) throw err;
   obj = JSON.parse(data);
 
-  console.log(obj.projects)
+  //console.log(obj.projects)
+
+  obj.projects.forEach(element => 
+    console.log(element+"ji")
+  );
 });
