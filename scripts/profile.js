dce36fb97b09bc4b4998132d6e02aecc9bd97896
Universe <williamsthehack@gmail.com>
2023-06-06 00:19:24 +0200

    Update profile.js

--- scripts/profile.js
+++ scripts/profile.js
@@ -8,16 +8,7 @@ module.exports = (req, res) => {
     console.log("user profile "+user_id+" request")
 
     db.query(
-        `SELECT
-
-        user_id,
-        avatar,
-        username,
-        color,
-        reactions,
-        background,
-        timestamp
-
+        `SELECT *
         FROM Users
         WHERE user_id = ${user_id}
         LIMIT 1`
@@ -34,12 +25,48 @@ module.exports = (req, res) => {
                 })
 
             } else {
-                res.status(200).json({
-                    success: true,
-                    error: false,
-                    message: "ok",
-                    profile: results
-                })
+                db.query(
+                    `SELECT *
+                    FROM Reactions
+                    WHERE user_id = ${user_id}
+                    LIMIT 50`
+                
+                    , function (error, react_results, fields) {
+                        if (error) {
+            
+                            console.error('error ' + error.message);
+            
+                            res.status(200).json({
+                                success: true,
+                                error: false,
+                                message: "ok",
+                                profile: {
+                                    user_id: results[0].user_id,
+                                    avatar: results[0].avatar,
+                                    username: results[0].username,
+                                    color: results[0].color,
+                                    background: results[0].background,
+                                    timestamp: results[0].timestamp
+                                }
+                            })
+            
+                        } else {
+                            res.status(200).json({
+                                success: true,
+                                error: false,
+                                message: "ok",
+                                profile: {
+                                    user_id: results[0].user_id,
+                                    avatar: results[0].avatar,
+                                    username: results[0].username,
+                                    color: results[0].color,
+                                    background: results[0].background,
+                                    timestamp: results[0].timestamp,
+                                    reactions: react_results
+                                }
+                            })
+                        }
+                });
             }
     });
 }
