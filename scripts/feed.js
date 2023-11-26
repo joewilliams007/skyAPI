15a5aec69c3a6148e8ba3f3560c5d2b18a4a36ed
joe <williamsthehack@gmail.com>
2023-11-26 18:32:16 +0100

    up

--- scripts/feed.js
+++ scripts/feed.js
@@ -51,7 +51,7 @@ module.exports = (req, res) => {
                     }
                     element.edited = !!element.edited === 1;
                     element.user_avatar = {}
-                    if (element.b==null) {
+                    if (element.b=="undefined") {
                         element.b="7bc8a4"
                     }
                     element.user_avatar.b = element.b;
